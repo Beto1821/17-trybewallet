93c7ee850ce9f6f79b1ef31e8110996972aa474d
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-06-22 19:53:46 -0300

    req01 rev5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setUserInfoAction } from '../actions/index';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  onSubmit = () => {
+    const { history, dispatchUserInfo } = this.props;
+    const { email } = this.state;
+    dispatchUserInfo(email);
+    history.push('/carteira');
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  emailValidation = () => {
+    const { email } = this.state;
+    const regex = /\w+@[a-z]+.com/g;
+    return regex.test(String(email).toLowerCase());
+  }
+
   render() {
-    return <div>Login</div>;
+    const minPasswordLength = 6;
+    const { password } = this.state;
+    return (
+      <section>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              data-testid="email-input"
+              name="email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="text"
+              data-testid="password-input"
+              name="password"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ !this.emailValidation() || password.length < minPasswordLength }
+            onClick={ this.onSubmit }
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  dispatchUserInfo: (email) => dispatch(setUserInfoAction(email)),
+});
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  dispatchUserInfo: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
-
 import user from './user';
 import wallet from './wallet';
 
-const rootReducer = combineReducers({ myReducer });
+// Mude esse reducer para algo que faça sentido com o seu projeto;
+// Lembrando que ele pode ficar em outro arquivo também;
+
+const rootReducer = combineReducers({ user, wallet });
 
 export default rootReducer;
-
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
