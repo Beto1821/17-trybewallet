7681330865e3a8e78aa1c42769210d046190e178
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-06-22 14:51:17 -0300

    req01 rev01

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import store from './redux/store/store';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,4 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
